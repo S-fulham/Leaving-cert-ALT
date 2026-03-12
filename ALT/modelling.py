9c1fec198d81ac4bdcfe051b5d793602f3c85f69
Sam Fulham <sam.fulham@letss.ie>
2026-03-12 10:01:37 +0000

    continued working on HTML

--- ALT/modelling.py
+++ ALT/modelling.py
@@ -4,20 +4,38 @@
 import random
 import time
 import copy
+import serial
+
+ser = serial.Serial("COM14", 115200)
+time.sleep(3)
+line = ser.readline().decode().strip()
+parts = line.split(",")
+Flame = 0
+Temperature = 0 
+soilMoisture = 0
+airHumidity = 0
+
+def getValues (Flame, Temperature, soilMoisture, airHumidity):
+    Flame.append(int(parts[1].strip()))
+    Temperature.append(int(parts[2].strip()))
+    soilMoisture.append(int(parts[3].strip()))
+    airHumidity.append(int(parts[3].strip()))
+    return Flame, Temperature, soilMoisture, airHumidity
 
 #creats the 2D grid
 WIDTH = 20
 HEIGHT = 20
 forest = [["T" for i in range (HEIGHT)] for i in range(WIDTH)]
 #variables
-moistureLevel = 0.3
+
+moistureLevel = soilMoisture
 tempurture = 31
-realFire = 1 - (moistureLevel)
+#realFire = 1 - (moistureLevel)
 
 # IF TEMP IS ABOVE 30 DEGREES CELSIUS WHICH IUS THE DANGER ZONE FOR FIRES THE MOSTURE LEVEL IS REDUCED (I WANT TO MAKE THIS CHANGE THE FIRE CHANCE NOT THE MOISUTE LEVEL BECAUSE THE MOISTURE LEVEL ISNT ACTUALLY CHANGING THE CHANCE OF FORE IS JUST INCREASING)
 #
-if tempurture > 30:
-    moistureLevel *= 0.9
+#if tempurture > 30:
+ #   moistureLevel *= 0.9
 
 #prints the forest
 def printForest(forest):
